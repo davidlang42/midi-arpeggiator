bbf96303a96a056589237d2b21571c51ba9a746e
David Lang <davidlang42@gmail.com>
2023-01-26 21:45:24 +1100

    add mode arg

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ mod arpeggio;
 mod arpeggiator;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 3 {
-        let midi_in = midi::InputDevice::open(&args[1])?;
-        let midi_out = midi::OutputDevice::open(&args[2])?;
-        let mut arp = arpeggiator::RepeatRecorder::new(midi_in, midi_out);
-        arp.listen();
-    } else {
-        println!("Requires exactly 2 arguments: MIDI_IN, MIDI_OUT");
-    }
+    let mut args = env::args().skip(1);
+    let mode = args.next().expect("The first argument should be the arpeggiator mode: repeat, pedal");
+    let midi_in = midi::InputDevice::open(&args.next().expect("The second argument should be the MIDI IN device file"))?;
+    let midi_out = midi::OutputDevice::open(&args.next().expect("The third argument should be the MIDI OUT device file"))?;
+    let mut arp: Box<dyn Arpeggiator> = match mode.as_str() {
+        "repeat" => Box::new(arpeggiator::RepeatRecorder::new(midi_in, midi_out)),
+        "pedal" => Box::new(arpeggiator::PedalRecorder::new(midi_in, midi_out)),
+        _ => panic!("Invalid arpeggiator mode: {}", mode)
+    };
+    arp.listen();
     Ok(())
 }
